f7af1e6468d160ab746772f08966ff810c9a6127
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2023-04-13 13:50:34 +0100

    navbar changes

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,5 +1,15 @@
-$(".menu-toggle").click(function () {
-    $(".nav").toggleClass("mobile-nav");
-    $(this).toggleClass("is-active");
-  });
-// Navbar  
+//Toggle navbar
+let navbarCollapseBtn = document.getElementById("navbar-collapse");
+let navbarCollapsable = document.querySelector(".navbar-collapsable");
+
+navbarCollapseBtn.addEventListener("click", () => {
+  if(navbarCollapsable.classList.contains("active")) {
+    document.body.classList.remove("stop")
+    navbarCollapseBtn.classList.replace("fa-xmark", "fa-bars");
+    navbarCollapsable.classList.remove("active")
+  }else {
+    document.body.classList.add("stop")
+    navbarCollapseBtn.classList.replace("fa-bars", "fa-xmark");
+    navbarCollapsable.classList.add("active")
+  }
+})
