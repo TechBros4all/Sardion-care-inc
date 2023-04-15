62f51b3071e95c6e41bb10cdf1525acd0472cc95
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2023-04-15 15:31:06 +0100

    massive changes to navbar unfinished though!!

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -2,16 +2,16 @@
 var links = document.querySelectorAll("a:not([href])");
 
 for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", () => {
-        let name = links[i].innerHTML.toLowerCase().split(" ");
-        if(name[0] == "contact") {
-          document.documentElement.scrollTop = document.documentElement.scrollHeight;
-          return;
-        }
-        let page = document.getElementById(`${name[0]}`);
-        if (page) page.scrollIntoView();
-        else return;
-    })
+  links[i].addEventListener("click", () => {
+    let name = links[i].innerHTML.toLowerCase().split(" ");
+    if (name[0] == "contact") {
+      document.documentElement.scrollTop = document.documentElement.scrollHeight;
+      return;
+    }
+    let page = document.getElementById(`${name[0]}`);
+    if (page) page.scrollIntoView();
+    else return;
+  })
 }
 
 //add active to links on poage scroll
@@ -24,13 +24,13 @@ function addActiveClass() {
     const halfHeight = window.innerHeight / 2;
 
     if (rect.top <= halfHeight && rect.bottom >= halfHeight) {
-      if(links[index].classList.contains("active")) return;
+      if (links[index].classList.contains("active")) return;
       links.forEach((link) => link.classList.remove('active'));
       links[index].classList.add('active');
     }
 
     if ((document.body.offsetHeight - (window.innerHeight + window.pageYOffset)) <= threshold) {
-      if(links[links.length - 1].classList.contains("active")) return;
+      if (links[links.length - 1].classList.contains("active")) return;
       links.forEach((link) => link.classList.remove('active'));
       links[links.length - 1].classList.add('active');
     }
@@ -40,17 +40,84 @@ function addActiveClass() {
 //Toggle navbar
 let navbarCollapseBtn = document.getElementById("navbar-collapse");
 let navbarCollapsable = document.querySelector(".navbar-collapsable");
+let navbarOverlay = document.querySelector(".navbar-overlay");
+let pageMenuBtn = document.querySelector(".page-menu-btn");
+let pageMenus = document.querySelector(".page-menus");
+let pageMenuLinks = document.querySelectorAll(".page-menus a");
 
-navbarCollapseBtn.addEventListener("click", () => {
-  if(navbarCollapsable.classList.contains("active")) {
+navbarCollapseBtn.addEventListener("click", function () {
+  if (navbarCollapsable.classList.contains("on")) {
+    //add other menu 
+    pageMenus.classList.remove("off");
+    //let body scroll
     document.body.classList.remove("stop")
+    //change icon
     navbarCollapseBtn.classList.replace("fa-xmark", "fa-bars");
-    navbarCollapsable.classList.remove("active")
-  }else {
+    //remove navbar
+    navbarCollapsable.classList.remove("on");
+    setTimeout(() => {
+      navbarOverlay.classList.remove("on", "dark");
+    }, 500);
+  } else {
+    //remove other menu 
+    pageMenus.classList.add("off");
+    //stop body scroll
     document.body.classList.add("stop")
+    //change icon
     navbarCollapseBtn.classList.replace("fa-bars", "fa-xmark");
-    navbarCollapsable.classList.add("active")
+    //show navbar
+    navbarCollapsable.classList.add("on")
+    navbarOverlay.classList.add("on", "dark");
   }
 })
+
+//toggle additional menu
+navbarOverlay.addEventListener("click", function () {
+  let children = this.querySelectorAll(`.navbar-overlay > div`);
+  //change icon
+  navbarCollapseBtn.classList.replace("fa-xmark", "fa-bars");
+  for (let i = 0; i < children.length; i++) {
+    children[i].classList.remove("on", "off");
+  }
+  setTimeout(() => {
+    this.classList.remove("on", "dark");
+  }, 500);
+})
+
+pageMenuBtn.addEventListener("click", function () {
+  delayAllFunc(pageMenuLinks, 0.2);
+  let icon = this.firstChild;
+  if (icon.classList.contains("fa-angle-up")) {
+    //remove other menu
+    navbarCollapsable.classList.add("off");
+
+    icon.classList.replace("fa-angle-up", "fa-angle-down")
+    navbarOverlay.classList.add("on");
+    pageMenus.classList.add("on");
+  } else {
+    //show other menus
+    navbarCollapsable.classList.remove("off");
+
+    icon.classList.replace("fa-angle-down", "fa-angle-up")
+    pageMenus.classList.remove("on");
+    setTimeout(() => {
+      navbarOverlay.classList.remove("on");
+    }, 500);
+  }
+})
+
+function delayAllFunc(val, num, end) {
+  let delay = 0;
+  for (let i = 0; i < val.length; i++) {
+    val[i].style.transitionDelay = `${delay}s`;
+    delay += num;
+  }
+  delay = end || 0;
+  setTimeout(() => {
+    for (let i = 0; i < val.length; i++) {
+      val[i].style.transitionDelay = `${delay}s`;
+    }
+  }, 2000)
+}
 
 window.addEventListener("scroll", addActiveClass)
