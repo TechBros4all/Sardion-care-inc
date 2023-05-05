7c2ea73c8c2bafc3a91ed3cb9926bf2edd74e9ce
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-05-05 11:35:06 +0100

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const clientRoutes = require("./routes/clientRoutes");
 const app = express();
-const port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static("resources"))
 
