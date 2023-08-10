92d4ae48b493d5c3ad94baf423d25b3c23f896ec
Samuel Adekunle <110774881+weird-samuel@users.noreply.github.com>
2023-08-10 09:24:44 +0100

    sitemap

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const clientRoutes = require("./routes/clientRoutes");
 const app = express();
+const path = require('path');
 var port = process.env.PORT || 3000;
 
 app.use(express.static("resources"))
@@ -9,6 +10,11 @@ app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.use(clientRoutes)
+
+app.get("/sitemap.xml", (req, res) => {
+    const filePath = path.join(__dirname, "sitemap.xml");
+    res.sendFile(filePath);
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
